a91b222c1a953fce727ed0563765915154837d0e
Recep KIRTAY <recepkirtay23@hotmail.com>
2023-05-09 00:56:45 +0300

    KDV Tutarı Hesaplayan Program

---
+++ src/KDVTutariHesaplayanProgram.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class KDVTutariHesaplayanProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float kdv;
+        System.out.print("Miktar giriniz: ");
+        int input = scanner.nextInt();
+
+        kdv = (input > 1000) ? 0.08f : 0.18f;
+        System.out.println("KDV Tutarı : " + (input*kdv));
+        System.out.println("KDV'li Fiyat : " + (input*(kdv+1)));
+    }
+}
